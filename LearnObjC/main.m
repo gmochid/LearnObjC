a0315627a856260bb684cd98ea799ae4ef3288e2
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2014-06-06 11:26:32 +0800

    Initial Commit

---
+++ LearnObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearnObjC
+//
+//  Created by gmochid on 6/6/14.
+//  Copyright (c) 2014 gmochid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
